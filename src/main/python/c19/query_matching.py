657a150d1af9e9c1d4d2704b40be19d0cbb21d40
Dynomant, Emeric <emeric.dynomant@atos.net>
2020-04-07 23:48:29 +0200

    Add sentence handling class

--- src/main/python/c19/query_matching.py
+++ src/main/python/c19/query_matching.py
@@ -7,7 +7,7 @@ import sqlite3
 import time
 from operator import itemgetter
 from typing import Any, List, Tuple
-
+from datetime import datetime
 import numpy as np
 import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
@@ -15,6 +15,43 @@ from sklearn.metrics.pairwise import cosine_similarity
 from c19.database_utilities import get_sentences
 from c19.embedding import Embedding
 from c19.text_preprocessing import preprocess_text
+
+
+from dateutil import parser
+
+
+class Sentence:
+    """
+    Proper object to hold data about top-k related sentences.
+    """
+    def __init__(self, sentence: List[Any], article: List[Any]) -> None:
+        # DOI of the article containing this sentence
+        self.doi: str = sentence[0]
+        # Section of the article  containing this sentence
+        self.section: str = sentence[1]
+        # The raw sentence as a str
+        self.sentence: str = sentence[2]
+        # The pre-processed sentence as a list of tokens
+        self.preprocessed_sentence: List[str] = sentence[3]
+        # The corresponding vector
+        self.vector: List[float] = sentence[4]
+        # Distance between this sentence and the query
+        self.distance: float = sentence[5]
+        # Publish date
+        self.date: datetime = parser.parse(article[1])
+
+        # Do we need it?
+
+        # Body of the article containing this sentence
+        # self.body: str = article[2]
+        # Abstract of the article containing this sentence
+        # self.abstract: str = article[3]
+        # Tiel  of the article containing this sentence
+        self.title: str = article[4]
+        # SHA signature of the JSON file
+        self.sha: str = article[5]
+        # Source of the publication
+        self.licence = article[6]
 
 
 def vectorize_query(embedding_model: Embedding, query: str) -> List[float]:
@@ -36,7 +73,7 @@ def vectorize_query(embedding_model: Embedding, query: str) -> List[float]:
     return query_vector
 
 
-def get_article(db_path: str, paper_doi) -> List[Any]:
+def get_article(db_path: str, paper_doi: str) -> List[Any]:
     """
     Retrieve an article by its paper_doi.
 
@@ -88,6 +125,7 @@ def get_sentences_data(
 
 
 def get_k_closest_sentences(query: str,
+                            db_path: str,
                             sentences: List[Any],
                             embedding_model: Embedding,
                             k: int = 10) -> Any:
@@ -107,13 +145,18 @@ def get_k_closest_sentences(query: str,
     # Vectorize it and format as arguments to be mapped by mp.Pool
     query_vector = list(vectorize_query(embedding_model, query))
     # Cosine sim of 1 means same vector, -1 opposite
-    distances = cosine_similarity([query_vector],
-                                  [sentence[4] for sentence in sentences])[0]
+    distances = cosine_similarity([query_vector], [sentence[4] for sentence in sentences])[0]
     for sentence, distance in zip(sentences, distances):
         sentence.append(distance)
     del distances
-    # Now, index 5 contains distance
+    # Now, index 5 contains distance and instance Sentence objects
     sentences = sorted(sentences, key=itemgetter(5), reverse=True)[0:k]
+    # May be long
+    sentences = [
+        Sentence(sentence, get_article(db_path, sentence[0]))
+        for sentence in sentences
+    ]
+
     toc = time.time()
     print(f"Took {round((toc-tic) / 60, 2)} minutes to process the query.")
 
