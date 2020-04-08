524dcdef2ed42b7b1b42a4af3a51895141eba215
GARCIA GARCIA <miguel.garciagarcia@atos.net>
2020-04-08 19:30:11 +0200

    Language detection with langdect instead of textblob

--- src/main/python/c19/language_detection.py
+++ src/main/python/c19/language_detection.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from textblob import TextBlob
+from langdetect import detect
+
 
 
 def get_lang(text: str) -> str:
@@ -19,6 +21,29 @@ def get_lang(text: str) -> str:
     if len(text) >= 3:
         blob = TextBlob(text)
         return blob.detect_language()
+    else:
+        raise ValueError(
+            "A minimum of 3 characters are needed for language detection!")
+
+
+
+def get_pylang(text: str) -> str:
+    """
+    Detects language of text: must contain minimum 3 characters.
+
+    Args:
+        text (str): The text from which language should be detected.
+
+    Raises:
+        ValueError: The provided string is not long enough.
+
+    Returns:
+        str: [description]
+    """
+    if len(text) >= 3:
+        lang = detect(text)
+        return lang
+
     else:
         raise ValueError(
             "A minimum of 3 characters are needed for language detection!")
