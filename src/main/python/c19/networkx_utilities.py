61ff7b2d591fd4941128f78d2adb2bc56f7d3ed4
baptiste <baptiste.metge@atos.net>
2020-04-15 17:47:28 +0200

    minor fix

--- src/main/python/c19/networkx_utilities.py
+++ src/main/python/c19/networkx_utilities.py
@@ -17,7 +17,7 @@ def get_citations_graph(urls = None) -> nx.DiGraph:
     dataframe = pd.concat((pd.read_csv(f, compression='zip')[['title', 'citation']] for f in all_files))
     # dataframe = pd.read_csv(file_path, compression='zip')[['title', 'citation']]
     G = nx.from_pandas_edgelist(dataframe,source='title',target='citation',create_using=nx.DiGraph )
-    print(f"Graph loaded is having {len(list(G.nodes))} nodes and {len(list(G.edges))} edges")
+    print(f"Full citation graph loaded is having {len(list(G.nodes))} nodes and {len(list(G.edges))} edges")
     return G
 
 def get_paper_cited_K_times_graph(G , M = 500) -> nx.DiGraph:
@@ -53,7 +53,7 @@ def add_pagerank_to_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
             for n in G.neighbors(title):
                 to_keep_nodes.append(n)
     Gsub = G.subgraph(to_keep_nodes)
-    print(f"Graph loaded is having {len(list(Gsub.nodes))} nodes and {len(list(Gsub.edges))} edges")
+    print(f"Reduce citation graph loaded is having {len(list(Gsub.nodes))} nodes and {len(list(Gsub.edges))} edges")
     # compute pagerank on network
     pr = nx.pagerank(Gsub)
     pagerank = pd.DataFrame(pr.items(), columns=["title", "pagerank"]).sort_values(by="pagerank", ascending=False)
